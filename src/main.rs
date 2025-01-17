141a1239cfc2c6abdb8da3d49f4fd4116cf4299c
Mathias Iversen <mathias@iversen.cc>
2025-01-17 15:50:16 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,10 @@ async fn index() -> Html<&'static str> {
 
 #[tokio::main]
 async fn main() {
-    // Build our application
     let app = Router::new()
         .route("/", get(index))
         .route("/healthcheck", get(|| async { "OK" }));
 
-    // Run it on 0.0.0.0:8000 to be accessible from outside the container
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     println!("Server running on http://0.0.0.0:8000");
     
